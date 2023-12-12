74ccfc29118fee39207cc018bae05833433bcc84
Marcel Martin <marcel.martin@scilifelab.se>
2023-12-12 20:03:09 +0100

    Inline the external top_expressed_genes.R script

--- sample_split.R
+++ sample_split.R
@@ -96,7 +96,17 @@ cat("Range for nCounts:", range(sdata$nCount_RNA), "\n")
 
 
 # Gene QC stats
-source("https://raw.githubusercontent.com/asabjorklund/single_cell_R_scripts/main/top_expressed_genes.R")
+
+# Boxplot with top expressed genes from a Seurat object.
+
+top_expressed_genes = function(sobject, nPlot=20, title="Top expressed genes", assay = "RNA"){
+  C = sobject@assays[[assay]]@counts
+  C@x = C@x/rep.int(colSums(C), diff(C@p))
+  most_expressed <- order(Matrix::rowSums(C), decreasing = T)[nPlot:1]
+  boxplot(t(as.matrix(C[most_expressed, ])), cex = 0.1, las = 1, xlab = "Fraction counts per cell",
+    col = (scales::hue_pal())(20)[20:1], horizontal = TRUE, main=title)
+  invisible(rownames(C)[most_expressed])
+}
 
 par(mar=c(5,6,2,2))
 top_expressed_genes(sdata)
